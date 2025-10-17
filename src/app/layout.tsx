82464a325e8d12e3bcd99bc5819b3faf38e835ae
Minigosha <margarita.lopez.romero@gmail.com>
2025-10-17 16:05:42 +0200

    dashboard v1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
         </Providers>
 
 
+
       </body>
     </html>
   );
---
+++ src/components/dashboard/user-data-card.tsx
@@ -0,0 +1,29 @@
+export type UserDataProps = {
+    name: string;
+    email: string;
+    image: unknown;
+    time: string;
+}
+
+export default function UserDataCard(props: UserDataProps) {
+    const defaultImage = "../../../public/user-icon.jpg"
+    return (
+        <div className="flex flex-wrap justify-between gap-3">
+            <section className="flex justify-between gap-3">
+                <div className="h-12 w-12rounded-full">
+                    <img width={300} height={300} src={props.image || defaultImage}
+                        alt="avatar" className="rounded-full h-12 w-12"></img>
+                </div>
+                <div className="text-sm">
+                    <p>{props.name}</p>
+                    <div className="text-ellipsis overflow-hidden whitespace-nowrap w-[120] sm:w-auto 
+                        opacity-30">
+                        {props.email}
+                    </div>
+                </div>
+            </section>
+            <p className="text-sm">{props.time}</p>
+        </div>
+    )
+
+}
