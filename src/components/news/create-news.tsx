4cc26ba709ef62272a53172c8f25e17de6948c68
Ahmed-Sayid <twakal222@gmail.com>
2025-10-23 10:05:52 +0200

    committ

---
+++ src/components/news/create-news.tsx
@@ -0,0 +1,25 @@
+
+"use  client";
+
+import { Card, CardHeader, CardTitle } from "../ui/card";
+
+export default function CreateNews(){
+
+return (
+
+    <>
+    <Card className="mb-6">
+        <CardHeader>
+            <CardTitle>
+                
+            </CardTitle>
+        </CardHeader>
+
+
+        
+    </Card>
+    
+    </>
+)
+
+}
